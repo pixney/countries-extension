ba39efe4866205f39c3fe4aefd8be055055c7a18
William <williamastrom@gmail.com>
2018-06-07 19:59:27 +0200

    Updated

--- src/Country/CountrySeeder.php
+++ src/Country/CountrySeeder.php
@@ -29,21 +29,21 @@ class CountrySeeder extends Seeder
         $this->countries = json_decode($this->files->get($this->file), 1);
         foreach ($this->countries as $key => $value) {
             $this->country->create([
-                'name'                            => $value['name'],
-                'capital'                         => (isset($value['capital'])) ? $value['capital'] : '',
-                'citizenship'                     => $value['citizenship'],
-                'country_code'                    => $value['country-code'],
-                'currency'                        => (isset($value['currency'])) ? $value['currency'] : '',
-                'currency_code'                   => (isset($value['currency_code'])) ? $value['currency_code'] : '',
-                'currency_sub_unit'               => (isset($value['currency_sub_unit'])) ? $value['currency_sub_unit'] : '',
-                'full_name'                       => (isset($value['full_name'])) ? $value['full_name'] : '',
-                'iso_3166_2'                      => $value['iso_3166_2'],
-                'iso_3166_3'                      => $value['iso_3166_3'],
-                'region_code'                     => $value['region-code'],
-                'sub_region_code'                 => $value['sub-region-code'],
-                'eea'                             => $value['eea'],
-                'currency_symbol'                 => (isset($value['currency_symbol'])) ? $value['currency_symbol'] : '',
-                'currency_decimals'               => (isset($value['currency_decimals'])) ? $value['currency_decimals'] : '',
+                'name'                            => $value['name'] ?? '',
+                'capital'                         => $value['capital'] ?? '',
+                'citizenship'                     => $value['citizenship'] ?? '',
+                'country_code'                    => $value['country-code'] ?? '',
+                'currency'                        => $value['currency'] ?? '',
+                'currency_code'                   => $value['currency_code'] ?? '',
+                'currency_sub_unit'               => $value['currency_sub_unit'] ?? '',
+                'full_name'                       => $value['full_name'] ?? '',
+                'iso_3166_2'                      => $value['iso_3166_2'] ?? '',
+                'iso_3166_3'                      => $value['iso_3166_3'] ?? '',
+                'region_code'                     => $value['region-code'] ?? '',
+                'sub_region_code'                 => $value['sub-region-code'] ?? '',
+                'eea'                             => $value['eea'] ?? '',
+                'currency_symbol'                 => $value['currency_symbol'] ?? '',
+                'currency_decimals'               => $value['currency_decimals'] ?? '',
             ]);
         }
     }
